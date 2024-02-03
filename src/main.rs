8c630d2046948d2d83da7e5001d3c651403debe8
Michael Gomez <m.nathan.gomez@gmail.com>
2024-02-02 22:27:24 -0800

    change project name, clean things up

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use actix_files::Files;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use tera::Tera;
+use actix_web::{web, App, HttpServer};
 
 mod routes;
 
@@ -9,11 +8,12 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(routes::load_templates()))
-            .service(Files::new("/static", "style").show_files_listing())
+            .service(Files::new("/static", "src/style").show_files_listing())
             .service(routes::home::home)
             .service(routes::blog::blog)
+            .service(routes::blog::blog_list)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:3000")?
     .run()
     .await
 }
--- src/routes/blog.rs
+++ src/routes/blog.rs
@@ -1,6 +1,31 @@
 use actix_web::{get, web, HttpRequest, HttpResponse, Responder};
 use tera::Tera;
 
+#[get("/blog")]
+pub async fn blog_list(tera: web::Data<Tera>) -> impl Responder {
+    // Read the file containing blog post titles
+    let html_files = vec![
+        "day1.html",
+        "evaluation_plan.html",
+        "metapost.html", /* add more files as needed */
+    ];
+
+    //strip the .html extension and underscores
+    let html_files = html_files
+        .iter()
+        .map(|x| x.replace("_", "").replace(".html", ""))
+        .collect::<Vec<String>>();
+
+    // Create a context with the list of blog posts
+    let mut context = tera::Context::new();
+    context.insert("html_files", &html_files);
+
+    // Render the blog list template
+    let rendered = tera.render("blog/blog_list.html", &context).unwrap();
+
+    HttpResponse::Ok().body(rendered)
+}
+
 #[get("/blog/{slug}")]
 pub async fn blog(req: HttpRequest, tera: web::Data<Tera>) -> impl Responder {
     // Extract the blog post slug from the request
