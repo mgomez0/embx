072f248e1db1cbdb6e38cff753937f0cc57bc68f
Michael Gomez <m.nathan.gomez@gmail.com>
2024-02-07 12:18:07 -0800

    add OG image

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(routes::load_templates()))
             .service(Files::new("/static", "src/style").show_files_listing())
+            .service(Files::new("/assets", "src/assets").show_files_listing())
             .service(routes::home::home)
             .service(routes::blog::blog)
             .service(routes::blog::blog_list)
