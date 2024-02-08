92ad1e21bb02e90d64d2bc8580a1a7a9ec16c922
Michael Gomez <m.nathan.gomez@gmail.com>
2024-02-08 12:01:05 -0800

    add logging to debug the issue

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use actix_files::Files;
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 
 mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // access logs are printed with the INFO level so ensure it is enabled by default
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .app_data(web::Data::new(routes::load_templates()))
             .service(Files::new("/static", "src/style").show_files_listing())
             .service(Files::new("/assets", "src/assets").show_files_listing())
